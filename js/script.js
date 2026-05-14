8f0980baa7972ac1185e3ce01703246c6a8e0f25
Brigil-star <brgil@nmsu.edu>
2026-05-14 12:23:44 -0600

    added javascript functionality, alert and hoover over function

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Alert when page loads
+window.onload = function() {
+    alert("Welcome to GreenTech Solutions website!");
+};
+
+
+document.querySelector("img").addEventListener("mouseover", function() {
+    this.title = "Our Solar panels convert sunlight into electricity, providing a clean and renewable energy source.";
+});
+
